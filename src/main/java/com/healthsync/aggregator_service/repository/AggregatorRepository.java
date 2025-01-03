f220742c2392a2d770a28c508038b8fdc96bd90e
Diwyangi Vithana <diwyangivithana1@gmail.com>
2025-01-03 12:06:36 +0300

    add new changes

--- src/main/java/com/healthsync/aggregator_service/repository/AggregatorRepository.java
+++ src/main/java/com/healthsync/aggregator_service/repository/AggregatorRepository.java
@@ -5,8 +5,8 @@ import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 import org.springframework.stereotype.Repository;
 
-import static com.healthsync.aggregator_service.service.impl.AggregatorServiceImpl.MONGO_DB_NAME;
-import static com.healthsync.aggregator_service.service.impl.AggregatorServiceImpl.MONGO_DB_URI;
+//import static com.healthsync.aggregator_service.service.impl.AggregatorServiceImpl.MONGO_DB_NAME;
+//import static com.healthsync.aggregator_service.service.impl.AggregatorServiceImpl.MONGO_DB_URI;
 
 /**
  * Repository class to interact with MongoDB collections.
@@ -17,17 +17,20 @@ public class AggregatorRepository {
 
     // Method to get MongoDB collection for doctors
     public MongoCollection<org.bson.Document> getDoctorCollection() {
-        return MongoClients.create(MONGO_DB_URI).getDatabase(MONGO_DB_NAME).getCollection("doctors");
+//        return MongoClients.create(MONGO_DB_URI).getDatabase(MONGO_DB_NAME).getCollection("doctors");
+          return null;
     }
 
     // Method to get MongoDB collection for appointments
     public MongoCollection<Document> getAppointmentCollection() {
-        return MongoClients.create(MONGO_DB_URI).getDatabase(MONGO_DB_NAME).getCollection("appointments");
+//        return MongoClients.create(MONGO_DB_URI).getDatabase(MONGO_DB_NAME).getCollection("appointments");
+        return null;
     }
 
     // Method to get MongoDB collection for patients
     public MongoCollection<Document> getPatientCollection() {
-        return MongoClients.create(MONGO_DB_URI).getDatabase(MONGO_DB_NAME).getCollection("patients");
+//        return MongoClients.create(MONGO_DB_URI).getDatabase(MONGO_DB_NAME).getCollection("patients");
+        return null;
     }
 
 
